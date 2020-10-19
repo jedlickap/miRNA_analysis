b630bda8dd478be32788b4854146003c807167c3
pavel <pavel@debian-10-buster.debian>
2020-10-19 20:26:01 +0200

    add next pyScript

---
+++ miRPara_miRnaLtrRt_intersecs_v1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#####
+# this program should detect relative positions of miRNAs within respective LTR retrotransposon
+# for each TE it will return rel. position (0.0-1.0 ~ start-end) of miRNAs for each TE part
+# 
+import sys
