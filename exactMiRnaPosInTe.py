6e470facdd2351f57af9b44dc5b9ef3502e2eadb
pavel <pavel@debian>
2020-11-06 16:45:15 +0100

    exactMiRnaPosInTe.py updated; nesterMergedGffOUTsParser_dualOut.py deleted

--- exactMiRnaPosInTe.py
+++ exactMiRnaPosInTe.py
@@ -17,6 +17,36 @@ def getTeHeaderList(miRnasProtDomBed):
     teHeadListUniq = list(set(teHeadList))
     return teHeadListUniq
 
+def antisenseRecalc(teDict):
+    antiFragList = list(teDict.keys())
+    antiFragList.reverse()
+    coordList = []
+    for k in teDict.keys():
+        coordList.append(teDict[k][1])
+        coordList.append(teDict[k][0])
+    teNewDict = {}
+    
+    for i in range(len(antiFragList)):
+        if i == 0:
+            teNewDict[antiFragList[i]] = []
+            teNewDict[antiFragList[i]].append(1)
+            teNewDict[antiFragList[i]].append(1 + (coordList[0] - coordList[1]))
+            lastValue = teNewDict[antiFragList[i]][1]
+            del coordList[0]
+        elif len(coordList) != 3:
+            teNewDict[antiFragList[i]] = []
+            teNewDict[antiFragList[i]].append((coordList[0] - coordList[1]) + lastValue)
+            teNewDict[antiFragList[i]].append((coordList[1] - coordList[2]) + teNewDict[antiFragList[i]][0])
+            lastValue = teNewDict[antiFragList[i]][1]
+            del coordList[0]
+            del coordList[0]
+        else:
+            teNewDict[antiFragList[i]] = []
+            teNewDict[antiFragList[i]].append((coordList[0] - coordList[1]) + lastValue)
+            teNewDict[antiFragList[i]].append((coordList[1] - coordList[2]) + teNewDict[antiFragList[i]][0])  
+    teDict = teNewDict
+    return teDict
+
 def fillTeDict(fragList, te, allProtDomTesGff):
     teDict = {}
     for i in range(len(fragList)):
@@ -29,43 +59,63 @@ def fillTeDict(fragList, te, allProtDomTesGff):
                     teDict[fragList[i]].append(int(l.split("\t")[4]))   
     # recalculate coordinates if antisense TE = True
     if teDict['ltr left'][0] > teDict['ltr right'][0]:
-        print("XXX")
-        antiFragList = list(teDict.keys())
-        coordList = []
-        for k in teDict.keys():
-            coordList.append(teDict[k][1])
-            coordList.append(teDict[k][0])
-        print("coordList", coordList)
-        teNewDict = {}
-        for i in range(len(antiFragList)):
-            lastValue = 0
-            if i == 0:
-                teNewDict[antiFragList[i]] = []
-                teNewDict[antiFragList[i]].append(1)
-                teNewDict[antiFragList[i]].append((coordList[0] - coordList[1]) + 1)
-                lastValue = teNewDict[antiFragList[i]][-1]
-                del coordList[0]
-            elif len(coordList) != 3:
-                teNewDict[antiFragList[i]] = []
-                teNewDict[antiFragList[i]].append((coordList[0] - coordList[1]) + lastValue)
-                teNewDict[antiFragList[i]].append((coordList[1] - coordList[2]) + teNewDict[antiFragList[i]][-1])
-                lastValue = teNewDict[antiFragList[i]][-1]
-                del coordList[0]
-            else:
-                teNewDict[antiFragList[i]] = []
-                teNewDict[antiFragList[i]].append((coordList[0] - coordList[1]) + lastValue)
-                teNewDict[antiFragList[i]].append(1)  
-        teDict = teNewDict      
+        teDict = antisenseRecalc(teDict)              
     return teDict
 
+def posFinder(start, end, teBasicDict):
+    found = False
+    outList = [] # fragName, fragS, fragE
+    fragList = list(teBasicDict.keys())
+    # check inside regular fragments
+    for k in teBasicDict.keys():
+        if not found:
+            if start >= teBasicDict[k][0] and end <= teBasicDict[k][1]:
+                found = True  
+                outList = [k, teBasicDict[k][0], teBasicDict[k][1]]
+                break
+    # check within the space between two adjacent fragments
+    # and
+    # check which two parts does TE miRNA intersect
+    if not found:
+        for i in range(len(fragList)):
+            if fragList[i] != fragList[-1]:
+                if teBasicDict[fragList[i]][1] <= start and end <= teBasicDict[fragList[i+1]][0]:                
+                    outList = [fragList[i] + "-" + fragList[i+1], teBasicDict[fragList[i]][1], 
+                    teBasicDict[fragList[i+1]][0]]
+                    found = True 
+                    break
+                elif teBasicDict[fragList[i]][1] > start and (teBasicDict[fragList[i]][1] < end < teBasicDict[fragList[i+1]][0]):
+                    outList = [fragList[i] + ":" + fragList[i] + "-" + fragList[i+1], 
+                    teBasicDict[fragList[i]][0], teBasicDict[fragList[i+1]][0]]
+                    found = True
+                    break
+                elif (teBasicDict[fragList[i]][1] < start < teBasicDict[fragList[i+1]][0]) and end > teBasicDict[fragList[i+1]][0]:
+                    outList = [fragList[i] + "-" + fragList[i+1] + ":" + fragList[i+1], 
+                    teBasicDict[fragList[i]][1], teBasicDict[fragList[i+1]][1]]
+                    found = True
+                    break                
+            else:
+                if start < teBasicDict[fragList[i]][0] and end < teBasicDict[fragList[i]][1]:
+                    outList = [fragList[i-1] + "-" + fragList[i] + ":" + fragList[i], 
+                    teBasicDict[fragList[i-1]][1], teBasicDict[fragList[i]][1]]
+                    found = True
+                    break
+    return "\t".join([str(elem) for elem in outList])   # join([str(elem) for elem in s]) 
+
+def miRnaCoordInTe(te, teBasicDict, miRnasProtDomBed):
+    teMiRnasOutList = []
+    miRnasList = []
+    with open(miRnasProtDomBed) as bed:
+        for rec in bed:
+            if te in rec:
+                miRnasList.append(rec)
+    for miRna in miRnasList:
+        start, end = int(miRna.split("\t")[1]), int(miRna.split("\t")[2])
+        teMiRnasOutList.append(miRna.split("\t")[0] + "\t" + posFinder(start, end, teBasicDict) + 
+        "\t" + str(start) + "\t" + str(end) + "\t" + miRna.split("\t")[3].rstrip())                                        # fragName, fragLen, miRnaRelPos
+    return teMiRnasOutList        
+
 def getMiRnaPos(miRnasProtDomBed, allProtDomTesGff):
-    # copiaListX = ['ltr left', 'ltr left-pbs', 'pbs', 'pbs-GAG', 'GAG', 'GAG-PROT', 'PROT', 'PROT-INT', 'INT', 
-    # 'INT-RT', 'RT', 'RT-RH', 'RH', 'RH-ppt', 'ppt', 'ppt-ltr right', 'ltr right']
-    # gypsyChrListX = ['ltr left', 'ltr left-pbs', 'pbs', 'pbs-GAG', 'GAG', 'GAG-PROT', 'PROT', 'PROT-RT', 'RT', 
-    # 'RT-RH', 'RH', 'RH-INT', 'INT', 'INT-CHD', 'CHD', 'CHD-ppt', 'ppt', 'ppt-ltr right', 'ltr right']
-    # gypsyNonChrListX = ['ltr left', 'ltr left-pbs', 'pbs', 'pbs-GAG', 'GAG', 'GAG-PROT', 'PROT', 'PROT-RT', 'RT', 
-    # 'RT-RH', 'RH', 'RH-INT', 'INT', 'INT-ppt', 'ppt', 'ppt-ltr right', 'ltr right']
-    
     # here I have to define which families belong to which superFams:
     famFragsDict = {
         'copia':[
@@ -80,24 +130,21 @@ def getMiRnaPos(miRnasProtDomBed, allProtDomTesGff):
             ['ltr left', 'pbs', 'GAG', 'PROT', 'RT', 'RH', 'INT', 'ppt', 'ltr right']]
             }
     teHeadList = getTeHeaderList(miRnasProtDomBed)
-    
+    outputList = []
+
+    # TODO: include output of all TEs fragments lengths and lengths of distances between them!
+
     for te in teHeadList:
-        # zjistiti fam a vytvorit dictionary
+        # which TE family and add specific fragments order
         fam = te.split("|")[3]        
         for superFam in sorted(famFragsDict.keys()):
             if fam in famFragsDict[superFam][0]:
+                # generate dictionary with all coordinates of TE fragments
                 teBasicDict = fillTeDict(famFragsDict[superFam][1], te, allProtDomTesGff)
-                for k in teBasicDict.keys():
-                    print(k, teBasicDict[k])
-
-    #     teFullDict = {}     # se zaclenenim prostoru mezi fragmenty a jejich koordinat
-
-    # with open(miRnasProtDomBed) as bed:
-    #     for rec in bed:
-    #         header = rec.split("\t")[0]
-    #         with open(allProtDomTesGff) as gff:
-    #             for l in gff:
-    #                 if header in l:
-    #                     print(header,l)
-
+                # here to add line prep for TE frag lengths!
+                outputList.append(miRnaCoordInTe(te, teBasicDict, miRnasProtDomBed))
+    for l1 in outputList:
+        for l2 in l1:
+            print(l2)
+                        
 getMiRnaPos(miRnasProtDomBed, allProtDomTesGff)
